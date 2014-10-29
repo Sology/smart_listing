811eac93eb79a9616d82e5c43acd76b2e74d7495
GCorbel <guirec.corbel@gmail.com>
2014-10-29 08:41:05 -0400

    Add spec to manage items

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
       get 'sortable'
     end
   end
+
+  namespace :admin do
+    resources :users
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
