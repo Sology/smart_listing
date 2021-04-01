edebbab0d75c30079ba01eaec5a0766969fde2ae
Łukasz Jachymczyk <lukasz@sology.eu>
2021-04-01 14:58:56 +0200

    Add microbundle setup.

--- app/javascript/smart_listing/controller.js
+++ app/javascript/smart_listing/controller.js
@@ -2,6 +2,8 @@ import { Controller } from 'stimulus';
 import SmartListingRegistry from './register';
 
 export default class extends Controller {
+  static targets = [ "name", "output", "boobzz" ]
+
   connect() {
     console.log(this);
     SmartListingRegistry.register(this.element);
