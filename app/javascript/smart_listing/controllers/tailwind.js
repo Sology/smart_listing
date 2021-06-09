f833ddb0ba2a920cc0ab57553754061527b5e00c
Łukasz Jachymczyk <lukasz@sology.eu>
2021-06-09 10:27:19 +0200

    Tweak tailwind opacity.

--- app/javascript/smart_listing/controllers/tailwind.js
+++ app/javascript/smart_listing/controllers/tailwind.js
@@ -8,10 +8,10 @@ export default class TailwindSmartListingController extends BaseController {
     console.log('hello from tailwind controller');
 
     this.element.addEventListener(eventsName.BEFORE_SEND, (e) => {
-      e.target.classList.add('opacity-20', 'pointer-events-none', 'transition-opacity');
+      e.target.classList.add('opacity-25', 'pointer-events-none', 'transition-opacity');
     });
     this.element.addEventListener(eventsName.AFTER_COMPLETE, (e) => {
-      e.target.classList.remove('opacity-20', 'pointer-events-none');
+      e.target.classList.remove('opacity-25', 'pointer-events-none');
     });
   }
 }
